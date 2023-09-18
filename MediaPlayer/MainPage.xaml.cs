b809814845f8250d5040249e754565bc8a4aec48
Kooper Young <61201332+Kupawaki@users.noreply.github.com>
2023-09-18 12:39:49 -0500

    Working color change button

--- MediaPlayer/MainPage.xaml.cs
+++ MediaPlayer/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using MediaPlayer.ViewModels;
+using Microsoft.Maui.Controls.PlatformConfiguration;
 using System.Diagnostics;
 
 namespace MediaPlayer
@@ -7,11 +8,20 @@ namespace MediaPlayer
     {
         bool isPressed = false;
 
+        Color color;
+        int r = 50;
+        int g = 255;
+        int b = 255;
+        bool x = false;
+
         public MainPage(MainViewModel mainViewModel)
         {
             InitializeComponent();
 
             BindingContext = mainViewModel;
+            color = Color.FromRgb(r, g, b);
+
+            colorFR.BackgroundColor = color;
         }
 
         private async void ColorButtonPressed(object sender, EventArgs e)
@@ -21,15 +31,31 @@ namespace MediaPlayer
             await Task.Run(() => {
                 while (isPressed)
                 {
-                    Debug.WriteLine($"Status is {isPressed}");
+                    //Debug.WriteLine($"Status is {isPressed}");
                     Thread.Sleep(500);
+
+                    Dispatcher.Dispatch(new Action(() =>
+                    {
+                        Debug.WriteLine("Dispatching...");
+                        if(x)
+                        {
+                            colorFR.BackgroundColor = Color.FromRgb(255, 0, 0);
+                            x = false;
+                        }
+                        else 
+                        {
+                            colorFR.BackgroundColor = Color.FromRgb(0, 255, 0);
+                            x = true;
+                        }
+                    }));
+
                     if(!isPressed)
                     {
-                        Debug.WriteLine("Thread should be finished");
                         return;
                     }
                 }
             });
+
         }
 
         private void ColorButtonReleased(object sender, EventArgs e)
